e1b33fe0597372bc670560ff357fa1fc186d248f
NE Consult <45170226+neconsult@users.noreply.github.com>
2026-09-17 18:12:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/consulta-anvisa', async (req, res) => {
 
     let browser;
     try {
-        const PROXY_HOST = "134.195.210.155";
+        const PROXY_HOST = "1.231.81.166";
         const PROXY_PORT = "3128";
 
         browser = await puppeteer.launch({
