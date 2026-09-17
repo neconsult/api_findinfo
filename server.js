825d9efa9560188759bdd9a8ae6fc3765d13f29a
NE Consult <45170226+neconsult@users.noreply.github.com>
2026-09-17 18:15:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.get('/teste', async (req, res) => {
 
     let browser;
     try {
-        const PROXY_HOST = "134.195.210.155";
+        const PROXY_HOST = "1.231.81.166";
         const PROXY_PORT = "3128";
 
         browser = await puppeteer.launch({
